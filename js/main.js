38bb423ecc8905e47ad59d54d0aa454402ff6d85
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-04-16 22:27:20 -0400

    create dropdown javascript for menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
     $('.menu-link').bigSlide();
+
+    $( "#menu-click" ).click(function() {
+    $( ".app" ).slideToggle( "slow" );
+    });
+
+    $( "#menu-click1" ).click(function() {
+    $( ".entrees" ).slideToggle( "slow" );
+    });
+
+    $( "#menu-click2" ).click(function() {
+    $( ".dessert" ).slideToggle( "slow" );
+    });
+
+    $( "#menu-click3" ).click(function() {
+    $( ".drinks" ).slideToggle( "slow" );
+    });
+
+    $( "#menu-click4" ).click(function() {
+    $( ".sides" ).slideToggle( "slow" );
+    });
 });
